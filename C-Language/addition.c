cae255825e5f697d8152c06bf4ef928b9a502f3c
lfmichel <lmichel.01@gmail.com>
2021-04-01 23:41:13 +0100

    Create addition file in C-Language folder

---
+++ C-Language/addition.c
@@ -0,0 +1,10 @@
+/* Program to addition two numbers */
+#include <stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("Enter two numbers: ");
+    scanf("%d, %d", &a, &b);
+    c=a+b;
+    printf("The sum is: %d", c);
+} 
