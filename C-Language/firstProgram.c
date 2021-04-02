9cc2c15173b4502a9d25c05afc91ba1fef1cda29
lfmichel <lmichel.01@gmail.com>
2021-04-02 17:21:34 +0100

    updated

--- C-Language/firstProgram.c
+++ C-Language/firstProgram.c
@@ -1,8 +1,8 @@
 /* Prints a message on the screen */
 #include <stdio.h>
-main()
+int main()
 {
-printf("Just one small step for coders. One giant leap for");
-printf(" programmers!\n");
-return 0;
+    printf("Just one small step for coders. One giant leap for");
+    printf(" programmers!\n");
+    return 0;
 }
